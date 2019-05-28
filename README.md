aac40d6112dc573e9fff8d7647ccae6bfa2a566c
mathieu.brunot <mb.mathieu.brunot@gmail.com>
2019-05-29 00:33:31 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+
+[uri_license]: http://www.gnu.org/licenses/agpl.html
+[uri_license_image]: https://img.shields.io/badge/License-AGPL%20v3-blue.svg
+
+[![License: AGPL v3][uri_license_image]][uri_license]
+[![Build Status](https://travis-ci.org/Monogramm/docker-erpnext.svg)](https://travis-ci.org/Monogramm/docker-erpnext)
+[![Docker Automated buid](https://img.shields.io/docker/build/monogramm/docker-erpnext.svg)](https://hub.docker.com/r/monogramm/docker-erpnext/)
+[![Docker Pulls](https://img.shields.io/docker/pulls/monogramm/docker-erpnext.svg)](https://hub.docker.com/r/monogramm/docker-erpnext/)
+[![](https://images.microbadger.com/badges/version/monogramm/docker-erpnext.svg)](https://microbadger.com/images/monogramm/docker-erpnext)
+[![](https://images.microbadger.com/badges/image/monogramm/docker-erpnext.svg)](https://microbadger.com/images/monogramm/docker-erpnext)
+
+# ERPNext Docker container
+
+Docker image for ERPNext.
+
+This image is directly inspired by [BizzoTech/docker-erpnext](https://github.com/BizzoTech/docker-erpnext) but adds an alpine variation, like provided by [donysukardi/docker-erpnext](https://github.com/donysukardi/docker-erpnext).
+
+:construction: **This image is still in development!**
+
+## What is Frappe ?
+
+Open Source ERP built for the web.
+
+> [erpnext.com](https://erpnext.com/)
+
+> [github erpnext](https://github.com/frappe/erpnext)
+
+## Supported tags
+
+https://hub.docker.com/r/monogramm/docker-erpnext/
+
+* TO BE COMPLETED
+
+# Questions / Issues
+If you got any questions or problems using the image, please visit our [Github Repository](https://github.com/Monogramm/docker-erpnext) and write an issue.  
---
+++ update.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -eo pipefail
+
+declare -A base=(
+	[stretch]='debian'
+	[stretch-slim]='debian'
+	[alpine]='alpine'
+)
+
+variants=(
+	stretch
+	stretch-slim
+	alpine
+)
+
+
+# version_greater_or_equal A B returns whether A >= B
+function version_greater_or_equal() {
+	[[ "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1" || "$1" == "$2" ]];
+}
+
+min_version=10.1
+
+dockerRepo="monogramm/docker-frappe"
+latestsFrappe=( $( curl -fsSL 'https://api.github.com/repos/frappe/erpnext/tags' |tac|tac| \
+	grep -oE '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | \
+	sort -urV ) 10.1.81 )
+
+latestsBench=( 4.1 master )
+
+# Remove existing images
+echo "reset docker images"
+find ./images -maxdepth 1 -type d -regextype sed -regex '\./images/[[:digit:]]\+\.[[:digit:]]\+' -exec rm -r '{}' \;
+
+echo "update docker images"
+travisEnv=
+for latest in "${latestsFrappe[@]}"; do
+	version=$(echo "$latest" | cut -d. -f1-2)
+
+	# Only add versions >= "$min_version"
+	if version_greater_or_equal "$version" "$min_version"; then
+
+		for variant in "${variants[@]}"; do
+			# Create the version+variant directory with a Dockerfile.
+			dir="images/$version/$variant"
+			if [ -d "$dir" ]; then
+				continue
+			fi
+			echo "generating frappe $latest [$version] ($variant)"
+			mkdir -p "$dir"
+
+			template="Dockerfile-${base[$variant]}.template"
+			cp "$template" "$dir/Dockerfile"
+
+			# Replace the variables.
+			sed -ri -e '
+				s/%%VARIANT%%/'"$variant"'/g;
+				s/%%VERSION%%/'"$latest"'/g;
+			' "$dir/Dockerfile"
+
+			# Copy the shell scripts
+			#for name in entrypoint; do
+			#	cp "docker-$name.sh" "$dir/$name.sh"
+			#	chmod 755 "$dir/$name.sh"
+			#done
+
+			cp ".dockerignore" "$dir/.dockerignore"
+
+			travisEnv='\n    - VERSION='"$version"' VARIANT='"$variant$travisEnv"
+
+			if [[ $1 == 'build' ]]; then
+				tag="$version-$variant"
+				echo "Build Dockerfile for ${tag}"
+				docker build -t ${dockerRepo}:${tag} $dir
+			fi
+		done
+
+	fi
+
+done
+
+# update .travis.yml
+travis="$(awk -v 'RS=\n\n' '$1 == "env:" && $2 == "#" && $3 == "Environments" { $0 = "env: # Environments'"$travisEnv"'" } { printf "%s%s", $0, RS }' .travis.yml)"
+echo "$travis" > .travis.yml
